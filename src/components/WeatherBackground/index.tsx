c243ef83942e3cf5f1ff42288d0fa3d03c893c50
PeterJFB <peterj.flbj@gmail.com>
2022-06-01 14:10:06 +0200

    Make terrible background transition not terrible

--- src/components/WeatherBackground/index.tsx
+++ src/components/WeatherBackground/index.tsx
@@ -1,10 +1,4 @@
-import React, {
-  FC,
-  TransitionEventHandler,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import React, { FC, TransitionEventHandler, useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import { WeatherHourData } from "../../types/weather";
 import CloudGroup from "./CloudGroup";
@@ -15,6 +9,7 @@ type Props = {
 type GradientProps = {
   hourData?: WeatherHourData;
   onTransitionEnd?: TransitionEventHandler<HTMLDivElement>;
+  visible?: boolean;
 };
 
 const calculateGradient = (hourData: WeatherHourData) => {
@@ -53,68 +48,37 @@ const LoadingGradient: FC<GradientProps> = ({ hourData }) => (
   />
 );
 
-const TransitionGradient: FC<GradientProps> = ({
-  hourData,
-  onTransitionEnd,
-}) => {
-  const [prevHourData, setPrevHourData] = useState<WeatherHourData>();
-  const [visible, setVisible] = useState(true);
-
-  useEffect(() => {
-    if (!prevHourData) return setPrevHourData(hourData);
-
-    setVisible(true);
-    setTimeout(() => {
-      setVisible(false);
-    }, 100);
-  }, [hourData]);
-
-  const gradient = prevHourData ? calculateGradient(prevHourData) : undefined;
-
-  console.log(visible, !!hourData);
-
+const WeatherGradient: FC<GradientProps> = ({ hourData }) => {
+  const gradient = hourData ? calculateGradient(hourData) : undefined;
   return (
     <Box
-      bgGradient={gradient}
       w="full"
       h="100%"
       pos="absolute"
       top={0}
       left={0}
-      transition={!visible ? "1s opacity" : "none"}
-      opacity={prevHourData && visible ? "100%" : "0%"}
-      zIndex={-3}
-      onTransitionEnd={() => {
-        setPrevHourData(hourData);
-      }}
+      bgGradient={gradient}
+      zIndex={-1}
     />
   );
 };
 
-const WeatherGradient: FC<GradientProps> = ({ hourData, onTransitionEnd }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  const [visible, setVisible] = useState(false);
+const WeatherToggleGradient: FC<GradientProps> = ({
+  hourData,
+  onTransitionEnd,
+  visible,
+}) => {
+  const [currentHourData, setCurrentHourData] = useState<WeatherHourData>();
 
   useEffect(() => {
-    (async () => {
-      setVisible(false);
-    })();
-    setTimeout(() => {
-      setVisible(true);
-    }, 100);
-  }, [hourData]);
+    if (visible) {
+      setCurrentHourData(hourData);
+    }
+  }, [visible]);
 
-  // useEffect(() => {
-  //   if (!visible)
-  //     (async () => {
-  //       setVisible(true);
-  //     })();
-  // }, [visible]);
-
-  const gradient = hourData ? calculateGradient(hourData) : undefined;
-
-  console.log(visible);
+  const gradient = currentHourData
+    ? calculateGradient(currentHourData)
+    : undefined;
 
   return (
     <Box
@@ -124,8 +88,7 @@ const WeatherGradient: FC<GradientProps> = ({ hourData, onTransitionEnd }) => {
       pos="absolute"
       top={0}
       left={0}
-      ref={ref}
-      transition={visible ? "1s opacity" : "none"}
+      transition="0.5s opacity"
       bgGradient={gradient}
       // bgGradient="linear(green.700, blue.900)"
       // bgGradient="linear(blue.100, blue.400)"
@@ -136,6 +99,12 @@ const WeatherGradient: FC<GradientProps> = ({ hourData, onTransitionEnd }) => {
 };
 
 const WeatherBackground: FC<Props> = ({ hourData }) => {
+  const [visibleToggle, setVisibleToggle] = useState(true);
+
+  useEffect(() => {
+    setVisibleToggle(!visibleToggle);
+  }, [hourData]);
+
   return (
     <Box
       pos="absolute"
@@ -147,12 +116,12 @@ const WeatherBackground: FC<Props> = ({ hourData }) => {
       overflowX="hidden"
     >
       <LoadingGradient hourData={hourData} />
-      <TransitionGradient hourData={hourData} />
       <WeatherGradient hourData={hourData} />
+      <WeatherToggleGradient hourData={hourData} visible={visibleToggle} />
+      <WeatherToggleGradient hourData={hourData} visible={!visibleToggle} />
       {hourData ? (
         <CloudGroup cloudAreaFraction={hourData.instant.cloudAreaFraction} />
       ) : null}
-      {/* <chakra.canvas /> */}
     </Box>
   );
 };
